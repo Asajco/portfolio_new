eed74f5452f3d28ef1ceaa8a31dade4dd1e94842
Asajco <petergacj@gmail.com>
2023-07-15 12:52:07 +0200

    progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,20 @@
 import React from 'react'
-import { Box, Flex, Button, chakra } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Button,
+  chakra,
+  useMediaQuery,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 import { Link } from 'react-router-dom'
 import ToggleColorButton from './ToggleColorButton'
 
@@ -14,7 +29,59 @@ function Header() {
       },
     },
   })
-  return (
+  const [isSmallerThan1200] = useMediaQuery('(max-width: 1200px)')
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+
+  return isSmallerThan1200 ? (
+    <Flex position="relative" flexDirection="column">
+      <Button
+        ref={btnRef}
+        onClick={onOpen}
+        w="10%"
+        alignSelf="flex-end"
+        m="0.25rem"
+      >
+        <HamburgerIcon />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size="full"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <Flex
+            fontFamily="roboto"
+            textDecoration="none"
+            flexDirection="column"
+            w="100%"
+            h="100%"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <CustomLink to="/" onClick={onClose}>
+              Home
+            </CustomLink>
+            <CustomLink
+              to="/projects"
+              style={{ margin: '1rem' }}
+              onClick={onClose}
+            >
+              Projects
+            </CustomLink>
+            <CustomLink to="/contact" mb="4rem" onClick={onClose}>
+              Contact me
+            </CustomLink>
+            <ToggleColorButton />
+          </Flex>
+        </DrawerContent>
+      </Drawer>
+    </Flex>
+  ) : (
     <Flex
       fontFamily="roboto"
       textDecoration="none"
--- src/components/ToggleColorButton.js
+++ src/components/ToggleColorButton.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Button, useColorMode } from '@chakra-ui/react'
+import { Button, useColorMode, useMediaQuery } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
 const ToggleColorButton = () => {
   const { colorMode, toggleColorMode } = useColorMode()
+  const [isSmallerThan1200] = useMediaQuery('(max-width: 1200px)')
   return (
     <Button
       onClick={() => toggleColorMode()}
-      position="absolute"
+      position={isSmallerThan1200 ? 'relative' : 'absolute'}
       top="0"
       right="0"
       m="1rem"
