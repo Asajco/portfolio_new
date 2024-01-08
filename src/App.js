2b5605443bd4defb806135a21ad1fd2340d79c40
Asajco <petergacj@gmail.com>
2024-01-08 16:37:16 +0100

    people count

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import { Box, Text } from '@chakra-ui/react'
+import { useEffect } from 'react'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Projects from './pages/Projects'
 import { Route, Router, Routes } from 'react-router'
 import { useContext } from 'react'
 import Contact from './pages/Contact'
+import { collection, getDoc, updateDoc, doc } from 'firebase/firestore'
+import { db } from './firebase/config'
 import Footer from './components/Footer'
 function App() {
+  useEffect(() => {
+    const checkAndSetCount = async () => {
+      const hasVisitedBefore = localStorage.getItem('hasVisitedBefore')
+
+      if (hasVisitedBefore) {
+      } else {
+        const documentId = 'WvYnzVwlfpEALwt6h3Cd'
+        // Increment the count in the database
+        try {
+          const viewCountDocRef = doc(db, 'viewCount', documentId)
+          // Replace 'your-document-id' with the actual document ID
+          const viewCountDocSnapshot = await getDoc(viewCountDocRef)
+
+          if (viewCountDocSnapshot.exists()) {
+            const viewCountData = viewCountDocSnapshot.data()
+            const currentCount = viewCountData.count || 0
+
+            // Update the count in the database
+            await updateDoc(viewCountDocRef, {
+              count: currentCount + 1,
+            })
+          } else {
+          }
+        } catch (error) {
+          console.error(error)
+        }
+
+        localStorage.setItem('hasVisitedBefore', true)
+      }
+    }
+
+    checkAndSetCount()
+  }, [])
+
   return (
     <Box>
       <Header />
---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import 'firebase/compat/auth'
+
+const app = firebase.initializeApp({
+  apiKey: 'AIzaSyA3lDtgtfAHqw1F82hztrjvN0ZgvuBMt7E',
+  authDomain: 'break-inventory.firebaseapp.com',
+  projectId: 'break-inventory',
+  storageBucket: 'break-inventory.appspot.com',
+  messagingSenderId: '136598683822',
+  appId: '1:136598683822:web:94e2b9b4f2da819a586d3f',
+  measurementId: 'G-V7WTVL4KT0',
+})
+
+// Initialize Firebase
+
+const analytics = getAnalytics(app)
+export const auth = app.auth()
+export const db = getFirestore(app)
+export default app
